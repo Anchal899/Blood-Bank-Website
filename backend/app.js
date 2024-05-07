9ad69b3ddf9f25452e26737bae5a43f34e724d8a
Anchal899 <101783656+Anchal899@users.noreply.github.com>
2024-05-07 15:17:53 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require('mongoose');
 const cookieParser = require("cookie-parser");
-
+const path = require('path');
 const app = express();
 const port = 3177;
 
